a98a344e86927577428e8289d48d4c30f9a756a1
Eduardo Pieretti Umpierre <eduardoumpierre@hotmail.com>
2018-04-23 16:09:47 -0300

    API endpoints added

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,63 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+/**
+ * Api
+ */
+$router->group(['prefix' => 'api/v1', 'middleware' => []], function () use ($router) {
+    /**
+     * Products
+     */
+    $router->group(['prefix' => 'products'], function () use ($router) {
+        $router->get('/', 'ProductController@getAll');
+        $router->get('/{id}', 'ProductController@getOne');
+        $router->post('/', 'ProductController@create');
+        $router->put('/{id}', 'ProductController@update');
+        $router->delete('/{id}', 'ProductController@delete');
+    });
+
+    /**
+     * Categories
+     */
+    $router->group(['prefix' => 'categories'], function () use ($router) {
+        $router->get('/', 'CategoryController@getAll');
+        $router->get('/{id}', 'CategoryController@getOne');
+        $router->post('/', 'CategoryController@create');
+        $router->put('/{id}', 'CategoryController@update');
+        $router->delete('/{id}', 'CategoryController@delete');
+    });
+
+    /**
+     * Ingredients
+     */
+    $router->group(['prefix' => 'ingredients'], function () use ($router) {
+        $router->get('/', 'IngredientController@getAll');
+        $router->get('/{id}', 'IngredientController@getOne');
+        $router->post('/', 'IngredientController@create');
+        $router->put('/{id}', 'IngredientController@update');
+        $router->delete('/{id}', 'IngredientController@delete');
+    });
+
+    /**
+     * Badges
+     */
+    $router->group(['prefix' => 'badges'], function () use ($router) {
+        $router->get('/', 'BadgeController@getAll');
+        $router->get('/{id}', 'BadgeController@getOne');
+        $router->post('/', 'BadgeController@create');
+        $router->put('/{id}', 'BadgeController@update');
+        $router->delete('/{id}', 'BadgeController@delete');
+    });
+
+    /**
+     * Users
+     */
+    $router->group(['prefix' => 'users'], function () use ($router) {
+        $router->get('/', 'UserController@getAll');
+        $router->get('/{id}', 'UserController@getOne');
+        $router->post('/', 'UserController@create');
+        $router->put('/{id}', 'UserController@update');
+        $router->delete('/{id}', 'UserController@delete');
+    });
+});
