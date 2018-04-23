eaff1cf6210b6cdfa913ac041fe68b1c7063d67d
Eduardo Pieretti Umpierre <eduardoumpierre@hotmail.com>
2018-04-23 16:03:23 -0300

    Categories tests added

---
+++ tests/Endpoints/CategoriesTest.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pichau
+ * Date: 23/04/2018
+ * Time: 15:19
+ */
+
+class CategoriesTest extends \TestCase
+{
+    const URL = '/api/v1/categories/';
+
+    /**
+     * Get all categories test
+     */
+    public function testGettingAllCategories()
+    {
+        // Get all categories with the authenticated user
+        $this->get(CategoriesTest::URL);
+        $this->assertResponseOk();
+
+        $this->seeJsonStructure([
+            '*' => [
+                'id', 'name'
+            ]
+        ]);
+    }
+
+    /**
+     * Get one category test
+     */
+    public function testGettingSpecificCategory()
+    {
+        // Get one category
+        $this->get(CategoriesTest::URL . '1');
+        $this->assertResponseStatus(200);
+
+        $this->seeJsonStructure([
+            'id', 'name'
+        ]);
+
+        // Accessing invalid category should give 404
+        $this->get(CategoriesTest::URL . '123456789');
+        $this->assertResponseStatus(404);
+    }
+
+    /**
+     * Creating category test
+     */
+    public function testCreatingCategory()
+    {
+        // Valid request
+        $this->post(CategoriesTest::URL, [
+            'name' => 'Category teste'
+        ]);
+        $this->assertResponseStatus(201);
+
+        // Invalid request - required fields are missing
+        $this->post(CategoriesTest::URL, [
+            'experience' => '1'
+        ]);
+        $this->assertResponseStatus(422);
+    }
+
+    /**
+     * Updating category test
+     */
+    public function testUpdatingCategory()
+    {
+        // Valid request
+        $this->put(CategoriesTest::URL . '1', [
+            'name' => 'Category teste'
+        ]);
+        $this->assertResponseStatus(200);
+
+        // Invalid request - required fields are missing
+        $this->put(CategoriesTest::URL . '1', []);
+        $this->assertResponseStatus(422);
+
+        // Invalid request - category do not exists
+        $this->put(CategoriesTest::URL . '4444', [
+            'name' => 'Category teste'
+        ]);
+        $this->assertResponseStatus(404);
+    }
+
+    /**
+     * Removing category test
+     */
+    public function testRemovingCategory()
+    {
+        // Valid request
+        $this->delete(CategoriesTest::URL . '1');
+        $this->assertResponseStatus(204);
+
+        // Invalid request - category don't exists
+        $this->delete(CategoriesTest::URL . '4444');
+        $this->assertResponseStatus(404);
+    }
+}
