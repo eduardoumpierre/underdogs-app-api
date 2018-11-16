84f2d0c264e216047422f544f7d579b5f4f79668
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2018-11-16 12:02:48 -0200

    User email unique validation removed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -128,7 +128,6 @@ class UserController extends Controller
     {
         $this->validate($request, [
             'name' => 'required',
-            'email' => 'nullable|unique:users',
             'facebook_id' => 'required:numeric'
         ]);
 
